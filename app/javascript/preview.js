6b6f1f9b3f157a97301e581706b8284cefb44b1b
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-03-19 23:27:38 +0900

    追加された画像とファイル選択ボタンに番号をつける

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const fileField = document.querySelector('input[type="file"][name="post[images][]"]');
   // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', function (e) {
+
+    // data-index（何番目を操作しているか）を取得
+    const dataIndex = e.target.getAttribute('data-index');
+
     // 古いプレビューが存在する場合は削除
     const alreadyPreview = document.querySelector('.preview');
     if (alreadyPreview) {
@@ -20,21 +24,30 @@ document.addEventListener('DOMContentLoaded', function () {
     // 画像を表示するためのdiv要素を生成
     const previewWrapper = document.createElement('div');
     previewWrapper.setAttribute('class', 'preview');
+    previewWrapper.setAttribute('data-index', dataIndex);
+
     // 表示する画像を生成
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src', blob);
+
     // 生成したHTMLの要素をブラウザに表示させる
     previewWrapper.appendChild(previewImage);
     previewList.appendChild(previewWrapper);
 
-    // 2枚目のfile_fieldを作成
+    // 2枚目用のfile_fieldを作成
     const newFileField = document.createElement('input');
     newFileField.setAttribute('type', 'file');
-    newFileField.setAttribute('mane', 'post[images][]');
+    newFileField.setAttribute('name', 'post[images][]');
+
+    // 最後のfile_fieldを取得
+    const lastFileField = document.querySelector('input[type="file"][name="post[images][]"]:last-child');
+    // nextDataIndex = 最後のfile_fieldのdata-index + 1
+    const nextDataIndex = Number(lastFileField.getAttribute('data-index')) + 1;
+    newFileField.setAttribute('data-index', nextDataIndex);
 
     // 生成したfile_fieldを表示
-    const fileFieldArea = document.querySelector('.click-upload');
-    fileFieldArea.appendChild(newFileField);
+    const fileFieldsArea = document.querySelector('.click-upload');
+    fileFieldsArea.appendChild(newFileField);
   });
 });
