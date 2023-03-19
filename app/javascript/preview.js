455bfa1a38eeb30bf7f4c4fd5eab103d8f27603a
hedgehog248 <atsushi.hasunuma02@gmail.com>
2023-03-20 00:19:19 +0900

    関数に切り分けてコードを整理する

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -6,21 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // 新規投稿・編集ページのフォームがないならここで終了
   if (!postForm) return null;
 
-  // input要素を取得
-  const fileField = document.querySelector('input[type="file"][name="post[images][]"]');
-  // input要素で値の変化が起きた際に呼び出される関数
-  fileField.addEventListener('change', function (e) {
-
-    // data-index（何番目を操作しているか）を取得
-    const dataIndex = e.target.getAttribute('data-index');
-
-    // 古いプレビューが存在する場合は削除
-    const alreadyPreview = document.querySelector('.preview');
-    if (alreadyPreview) {
-      alreadyPreview.remove();
-    };
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
+  // プレビュー画像を生成表示する関数
+  const buildPreviewImage = (dataIndex, blob) => {
     // 画像を表示するためのdiv要素を生成
     const previewWrapper = document.createElement('div');
     previewWrapper.setAttribute('class', 'preview');
@@ -34,7 +21,10 @@ document.addEventListener('DOMContentLoaded', function () {
     // 生成したHTMLの要素をブラウザに表示させる
     previewWrapper.appendChild(previewImage);
     previewList.appendChild(previewWrapper);
+  };
 
+  // file_fieldを生成・表示する関数
+  const buildNewFileField = () => {
     // 2枚目用のfile_fieldを作成
     const newFileField = document.createElement('input');
     newFileField.setAttribute('type', 'file');
@@ -49,5 +39,28 @@ document.addEventListener('DOMContentLoaded', function () {
     // 生成したfile_fieldを表示
     const fileFieldsArea = document.querySelector('.click-upload');
     fileFieldsArea.appendChild(newFileField);
-  });
+  };
+
+  // input要素で値の変化が起きた際に呼び出される関数の中身
+  const changedFileField = (e) => {
+    // data-index（何番目を操作しているか）を取得
+    const dataIndex = e.target.getAttribute('data-index');
+
+    // 古いプレビューが存在する場合は削除
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    buildPreviewImage(dataIndex, blob);
+    buildNewFileField();
+  };
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="post[images][]"]');
+
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', changedFileField);
 });
